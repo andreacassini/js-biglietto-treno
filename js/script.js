b4e0d401ec3a47b7077e074b9a2c2b86722cc973
andreacassini <andrea1316@hotmail.it>
2023-05-18 10:19:47 +0200

    ADD prezzo scontato e intero, output

--- js/script.js
+++ js/script.js
@@ -7,9 +7,22 @@ let age = prompt('Inserire la tua età');
 //Calcolare il prezzo del biglietto base tramite la forumla 0.21 * km
 let prezzo = 0.21 * km;
 
+let message;
+
 //SE l'età è minore di 18...
 if(age < 18){
     //Calcolare il prezzo scontato  
     prezzo = prezzo * 0.8;
     console.log(prezzo);
+    message = ` Hai diritto al prezzo scontato del 20%. Il prezzo del biglietto è di: ${prezzo.toFixed(2)}€`;
+} else if(age > 65){
+    //Calcolare il prezzo scontato
+    prezzo = prezzo * 0.6;
+    console.log(prezzo);
+    message = ` Hai diritto al prezzo scontato del 40%. Il prezzo del biglietto è di: ${prezzo.toFixed(2)}€`;
+} else{
+    message = `Il prezzo del biglietto è di: ${prezzo.toFixed(2)}€`;
 }
+
+//Mostrare il prezzo
+document.getElementById(`message`).innerHTML = message;
